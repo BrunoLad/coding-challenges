1097cc26f1b39fc10ebac72d40e5ac5b352e48df
Bruno Ladeia <bruno13dm@outlook.com>
2023-12-01 11:37:25 -0300

    Solve 106 - Trim Left challenge

---
+++ Type Challenges/Medium/106 - Trim Left.ts
@@ -0,0 +1,40 @@
+/*
+  106 - Trim Left
+  -------
+  by Anthony Fu (@antfu) #medium #template-literal
+
+  ### Question
+
+  Implement `TrimLeft<T>` which takes an exact string type and returns a new string with the whitespace beginning removed.
+
+  For example
+
+  ```ts
+  type trimed = TrimLeft<'  Hello World  '> // expected to be 'Hello World  '
+  ```
+
+  > View on GitHub: https://tsch.js.org/106
+*/
+
+/* _____________ Your Code Here _____________ */
+
+type TrimLeft<S extends string> = S extends ` ${infer X}`
+  ? TrimLeft<X>
+  : S extends `\n${infer X}`
+    ? TrimLeft<X>
+    : S extends `\t${infer X}`
+      ? TrimLeft<X>
+      : S;
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<TrimLeft<'str'>, 'str'>>,
+  Expect<Equal<TrimLeft<' str'>, 'str'>>,
+  Expect<Equal<TrimLeft<'     str'>, 'str'>>,
+  Expect<Equal<TrimLeft<'     str     '>, 'str     '>>,
+  Expect<Equal<TrimLeft<'   \n\t foo bar '>, 'foo bar '>>,
+  Expect<Equal<TrimLeft<''>, ''>>,
+  Expect<Equal<TrimLeft<' \n\t'>, ''>>,
+]
