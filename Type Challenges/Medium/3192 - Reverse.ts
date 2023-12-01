0c9d4e5d915083dae91e635c0fdb0a1c2fd29804
Bruno Ladeia <bruno13dm@outlook.com>
2023-12-01 14:31:08 -0300

    Solve 3192 - Reverse challenge

---
+++ Type Challenges/Medium/3192 - Reverse.ts
@@ -0,0 +1,38 @@
+/*
+  3192 - Reverse
+  -------
+  by jiangshan (@jiangshanmeta) #medium #tuple
+
+  ### Question
+
+  Implement the type version of ```Array.reverse```
+
+  For example:
+
+  ```typescript
+  type a = Reverse<['a', 'b']> // ['b', 'a']
+  type b = Reverse<['a', 'b', 'c']> // ['c', 'b', 'a']
+  ```
+
+  > View on GitHub: https://tsch.js.org/3192
+*/
+
+/* _____________ Your Code Here _____________ */
+
+type Reverse<T extends unknown[]> = T extends [...infer Rest, infer L] ? [L, ...Reverse<Rest>] : [];
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<Reverse<[]>, []>>,
+  Expect<Equal<Reverse<['a', 'b']>, ['b', 'a']>>,
+  Expect<Equal<Reverse<['a', 'b', 'c']>, ['c', 'b', 'a']>>,
+]
+
+type errors = [
+  // @ts-expect-error
+  Reverse<'string'>,
+  // @ts-expect-error
+  Reverse<{ key: 'value' }>,
+]
