d9fcd014f968512096e5ecbe15cbe59d441f3cab
Bruno Ladeia <bruno13dm@outlook.com>
2023-12-06 17:14:20 -0300

    Solve 8767 - Combination challenge

---
+++ Type Challenges/Medium/8767 - Combination.ts
@@ -0,0 +1,34 @@
+/*
+  8767 - Combination
+  -------
+  by Homyee King (@HomyeeKing) #medium #array #application #string
+
+  ### Question
+
+  Given an array of strings, do Permutation & Combination.
+  It's also useful for the prop types like video [controlsList](https://developer.mozilla.org/en-US/docs/Web/API/HTMLMediaElement/controlsList)
+
+  ```ts
+  // expected to be `"foo" | "bar" | "baz" | "foo bar" | "foo bar baz" | "foo baz" | "foo baz bar" | "bar foo" | "bar foo baz" | "bar baz" | "bar baz foo" | "baz foo" | "baz foo bar" | "baz bar" | "baz bar foo"`
+  type Keys = Combination<['foo', 'bar', 'baz']>
+  ```
+
+  > View on GitHub: https://tsch.js.org/8767
+*/
+
+/* _____________ Your Code Here _____________ */
+// First | `${First} ${Combination<Rest>}` | `${Combination<Rest>} ${First}` | Combination<Rest>
+type Combination<T extends string[]> = 
+  T extends [infer First extends string, ...infer Rest extends string[]]
+    ? T extends [...infer Head extends string[], infer Tail extends string]
+      ? First | `${First} ${Combination<Rest>}` | `${Combination<Rest>} ${First}` | `${Tail} ${Combination<Head>}` | `${Combination<Head>} ${Tail}` | Combination<Rest>
+      : never
+    : never;
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<Combination<['foo', 'bar', 'baz']>,
+  'foo' | 'bar' | 'baz' | 'foo bar' | 'foo bar baz' | 'foo baz' | 'foo baz bar' | 'bar foo' | 'bar foo baz' | 'bar baz' | 'bar baz foo' | 'baz foo' | 'baz foo bar' | 'baz bar' | 'baz bar foo'>>,
+]
