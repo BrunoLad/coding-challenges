6dda221a35eef7d549ac682cf8ac69dd98c3cc15
Bruno Ladeia <bruno13dm@outlook.com>
2023-12-06 21:35:22 -0300

    Solve 1097 - IsUnion challenge

---
+++ Type Challenges/Medium/1097 - IsUnion.ts
@@ -0,0 +1,43 @@
+/*
+  1097 - IsUnion
+  -------
+  by null (@bencor) #medium #union
+
+  ### Question
+
+  Implement a type `IsUnion`, which takes an input type `T` and returns whether `T` resolves to a union type.
+
+  For example:
+
+  ```ts
+  type case1 = IsUnion<string> // false
+  type case2 = IsUnion<string | number> // true
+  type case3 = IsUnion<[string | number]> // false
+  ```
+
+  > View on GitHub: https://tsch.js.org/1097
+*/
+
+/* _____________ Your Code Here _____________ */
+// Wasn't able to solve it, referencing this brilliant solution with explanations: https://github.com/type-challenges/type-challenges/issues/1140
+type IsUnionImpl<T, C extends T = T> = (T extends T ? C extends T ? true : unknown : never) extends true ? false : true;
+type IsUnion<T> = IsUnionImpl<T>
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<IsUnion<string>, false>>,
+  Expect<Equal<IsUnion<string | number>, true>>,
+  Expect<Equal<IsUnion<'a' | 'b' | 'c' | 'd'>, true>>,
+  Expect<Equal<IsUnion<undefined | null | void | ''>, true>>,
+  Expect<Equal<IsUnion<{ a: string } | { a: number }>, true>>,
+  Expect<Equal<IsUnion<{ a: string | number }>, false>>,
+  Expect<Equal<IsUnion<[string | number]>, false>>,
+  // Cases where T resolves to a non-union type.
+  Expect<Equal<IsUnion<string | never>, false>>,
+  Expect<Equal<IsUnion<string | unknown>, false>>,
+  Expect<Equal<IsUnion<string | any>, false>>,
+  Expect<Equal<IsUnion<string | 'a'>, false>>,
+  Expect<Equal<IsUnion<never>, false>>,
+]
