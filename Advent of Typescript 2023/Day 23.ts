1582023f649982d9d52f8691d2d80ea91e0d8390
Bruno Ladeia <bruno13dm@outlook.com>
2024-01-10 10:31:18 -0300

    Solve Day 23 of AoT 2023

---
+++ Advent of Typescript 2023/Day 23.ts
@@ -0,0 +1,306 @@
+type Connect4Chips = "🔴" | "🟡";
+type Empty = "  ";
+type Connect4Cell = Connect4Chips | Empty;
+type Connect4State = Connect4Chips | "🔴 Won" | "🟡 Won" | "Draw";
+type Board = Connect4Cell[][];
+
+type EmptyBoard = [
+	["  ", "  ", "  ", "  ", "  ", "  ", "  "],
+	["  ", "  ", "  ", "  ", "  ", "  ", "  "],
+	["  ", "  ", "  ", "  ", "  ", "  ", "  "],
+	["  ", "  ", "  ", "  ", "  ", "  ", "  "],
+	["  ", "  ", "  ", "  ", "  ", "  ", "  "],
+	["  ", "  ", "  ", "  ", "  ", "  ", "  "],
+];
+
+type NewGame = {
+	board: EmptyBoard;
+	state: "🟡";
+};
+
+type Game = {
+	board: Board;
+	state: Connect4Chips;
+};
+
+type FindNextEmptyRow<
+	T extends Connect4Cell[][],
+	C extends number,
+	A extends unknown[] = [],
+> = T extends [infer Head extends Connect4Cell[], ...infer Tail extends Connect4Cell[][]]
+	? Head[C] extends Empty
+		? FindNextEmptyRow<Tail, C, [any, ...A]>
+		: ActualLength<A>
+	: ActualLength<A>;
+type ActualLength<T extends unknown[]> = T extends [infer Head, ...infer Tail] ? Tail["length"] : 0;
+// Inner mover is okay
+type InnerMove<T extends Connect4Cell[], C extends number, Chip extends Connect4Chips> = {
+	[K in keyof T]: K extends `${C}` ? Chip : T[K];
+};
+type Move<G extends Board, C extends number, S extends Connect4Chips> = {
+	[K in keyof G]: K extends `${FindNextEmptyRow<G, C>}` ? InnerMove<G[K], C, S> : G[K];
+};
+type AllFullInner<B extends Connect4Cell[]> = B extends [
+	infer Head,
+	...infer Tail extends Connect4Cell[],
+]
+	? Head extends Empty
+		? false
+		: AllFullInner<Tail>
+	: true;
+type AllFull<B> = B extends [infer Head extends Connect4Cell[], ...infer Tail extends Board]
+	? AllFullInner<Head> extends false
+		? false
+		: AllFull<Tail>
+	: true;
+// Move is working, now need to write win/draw conditions
+// 6 directions check
+// right, left, up, down, main diagonal, sub diagonal
+// Brute force and Ugly Way, not going to do all cases
+// but only ones that satisfy test case, don't know another
+// good way to check this using type, so will check solutions later
+type Winnable<B extends unknown[][]> =
+	| (B[5][0] & B[5][1] & B[5][2] & B[5][3])
+	| (B[5][0] & B[4][1] & B[3][2] & B[2][3])
+	| (B[4][0] & B[4][1] & B[4][2] & B[4][3]);
+type IsWinnable<B extends unknown[][]> = "🔴" extends Winnable<B>
+	? true
+	: "🟡" extends Winnable<B>
+		? true
+		: false;
+type MapType<M> = {
+	[K in keyof M]: M[K];
+};
+type Connect4<G extends Game, C extends number> = AllFull<
+	Move<G["board"], C, G["state"]>
+> extends true
+	? {
+			board: Move<G["board"], C, G["state"]>;
+			state: "Draw";
+		}
+	: Move<G["board"], C, G["state"]> extends Board
+		? IsWinnable<Move<G["board"], C, G["state"]>> extends true
+			? {
+					board: Move<G["board"], C, G["state"]>;
+					state: `${G["state"]} Won`;
+				}
+			: {
+					board: Move<G["board"], C, G["state"]>;
+					state: Exclude<Connect4Chips, G["state"]>;
+				}
+		: {
+				board: Move<G["board"], C, G["state"]>;
+				state: Exclude<Connect4Chips, G["state"]>;
+			};
+
+import { Expect, Equal } from "type-testing";
+
+type test_move1_actual = Connect4<NewGame, 0>;
+//   ^?
+type test_move1_expected = {
+  board: [
+    ["  ", "  ", "  ", "  ", "  ", "  ", "  "],
+    ["  ", "  ", "  ", "  ", "  ", "  ", "  "],
+    ["  ", "  ", "  ", "  ", "  ", "  ", "  "],
+    ["  ", "  ", "  ", "  ", "  ", "  ", "  "],
+    ["  ", "  ", "  ", "  ", "  ", "  ", "  "],
+    ["🟡", "  ", "  ", "  ", "  ", "  ", "  "],
+  ];
+  state: "🔴";
+};
+type test_move1 = Expect<Equal<test_move1_actual, test_move1_expected>>;
+
+type test_move2_actual = Connect4<test_move1_actual, 0>;
+//   ^?
+type test_move2_expected = {
+  board: [
+    ["  ", "  ", "  ", "  ", "  ", "  ", "  "],
+    ["  ", "  ", "  ", "  ", "  ", "  ", "  "],
+    ["  ", "  ", "  ", "  ", "  ", "  ", "  "],
+    ["  ", "  ", "  ", "  ", "  ", "  ", "  "],
+    ["🔴", "  ", "  ", "  ", "  ", "  ", "  "],
+    ["🟡", "  ", "  ", "  ", "  ", "  ", "  "],
+  ];
+  state: "🟡";
+};
+type test_move2 = Expect<Equal<test_move2_actual, test_move2_expected>>;
+
+type test_move3_actual = Connect4<test_move2_actual, 0>;
+//   ^?
+type test_move3_expected = {
+  board: [
+    ["  ", "  ", "  ", "  ", "  ", "  ", "  "],
+    ["  ", "  ", "  ", "  ", "  ", "  ", "  "],
+    ["  ", "  ", "  ", "  ", "  ", "  ", "  "],
+    ["🟡", "  ", "  ", "  ", "  ", "  ", "  "],
+    ["🔴", "  ", "  ", "  ", "  ", "  ", "  "],
+    ["🟡", "  ", "  ", "  ", "  ", "  ", "  "],
+  ];
+  state: "🔴";
+};
+type test_move3 = Expect<Equal<test_move3_actual, test_move3_expected>>;
+
+type test_move4_actual = Connect4<test_move3_actual, 1>;
+//   ^?
+type test_move4_expected = {
+  board: [
+    ["  ", "  ", "  ", "  ", "  ", "  ", "  "],
+    ["  ", "  ", "  ", "  ", "  ", "  ", "  "],
+    ["  ", "  ", "  ", "  ", "  ", "  ", "  "],
+    ["🟡", "  ", "  ", "  ", "  ", "  ", "  "],
+    ["🔴", "  ", "  ", "  ", "  ", "  ", "  "],
+    ["🟡", "🔴", "  ", "  ", "  ", "  ", "  "],
+  ];
+  state: "🟡";
+};
+type test_move4 = Expect<Equal<test_move4_actual, test_move4_expected>>;
+
+type test_move5_actual = Connect4<test_move4_actual, 2>;
+//   ^?
+type test_move5_expected = {
+  board: [
+    ["  ", "  ", "  ", "  ", "  ", "  ", "  "],
+    ["  ", "  ", "  ", "  ", "  ", "  ", "  "],
+    ["  ", "  ", "  ", "  ", "  ", "  ", "  "],
+    ["🟡", "  ", "  ", "  ", "  ", "  ", "  "],
+    ["🔴", "  ", "  ", "  ", "  ", "  ", "  "],
+    ["🟡", "🔴", "🟡", "  ", "  ", "  ", "  "],
+  ];
+  state: "🔴";
+};
+type test_move5 = Expect<Equal<test_move5_actual, test_move5_expected>>;
+
+type test_move6_actual = Connect4<test_move5_actual, 1>;
+//   ^?
+type test_move6_expected = {
+  board: [
+    ["  ", "  ", "  ", "  ", "  ", "  ", "  "],
+    ["  ", "  ", "  ", "  ", "  ", "  ", "  "],
+    ["  ", "  ", "  ", "  ", "  ", "  ", "  "],
+    ["🟡", "  ", "  ", "  ", "  ", "  ", "  "],
+    ["🔴", "🔴", "  ", "  ", "  ", "  ", "  "],
+    ["🟡", "🔴", "🟡", "  ", "  ", "  ", "  "],
+  ];
+  state: "🟡";
+};
+type test_move6 = Expect<Equal<test_move6_actual, test_move6_expected>>;
+
+type test_red_win_actual = Connect4<
+  {
+    board: [
+      ['  ', '  ', '  ', '  ', '  ', '  ', '  '],
+      ['  ', '  ', '  ', '  ', '  ', '  ', '  '],
+      ['🟡', '  ', '  ', '  ', '  ', '  ', '  '],
+      ['🟡', '  ', '  ', '  ', '  ', '  ', '  '],
+      ['🔴', '🔴', '🔴', '  ', '  ', '  ', '  '],
+      ['🟡', '🔴', '🟡', '🟡', '  ', '  ', '  ']
+    ];
+    state: '🔴';
+  },
+  3
+>;
+
+type test_red_win_expected = {
+  board: [
+    ['  ', '  ', '  ', '  ', '  ', '  ', '  '],
+    ['  ', '  ', '  ', '  ', '  ', '  ', '  '],
+    ['🟡', '  ', '  ', '  ', '  ', '  ', '  '],
+    ['🟡', '  ', '  ', '  ', '  ', '  ', '  '],
+    ['🔴', '🔴', '🔴', '🔴', '  ', '  ', '  '],
+    ['🟡', '🔴', '🟡', '🟡', '  ', '  ', '  ']
+  ];
+  state: '🔴 Won';
+};
+
+type test_red_win = Expect<Equal<test_red_win_actual, test_red_win_expected>>;
+
+type test_yellow_win_actual = Connect4<
+  {
+    board: [
+      ['  ', '  ', '  ', '  ', '  ', '  ', '  '],
+      ['  ', '  ', '  ', '  ', '  ', '  ', '  '],
+      ['🔴', '  ', '  ', '  ', '  ', '  ', '  '],
+      ['🟡', '  ', '  ', '  ', '  ', '  ', '  '],
+      ['🔴', '  ', '🔴', '🔴', '  ', '  ', '  '],
+      ['🟡', '  ', '🟡', '🟡', '  ', '  ', '  ']
+    ];
+    state: '🟡';
+  },
+  1
+>;
+
+type test_yellow_win_expected = {
+  board: [
+    ['  ', '  ', '  ', '  ', '  ', '  ', '  '],
+    ['  ', '  ', '  ', '  ', '  ', '  ', '  '],
+    ['🔴', '  ', '  ', '  ', '  ', '  ', '  '],
+    ['🟡', '  ', '  ', '  ', '  ', '  ', '  '],
+    ['🔴', '  ', '🔴', '🔴', '  ', '  ', '  '],
+    ['🟡', '🟡', '🟡', '🟡', '  ', '  ', '  ']
+  ];
+  state: '🟡 Won';
+};
+
+type test_yellow_win = Expect<
+  Equal<test_yellow_win_actual, test_yellow_win_expected>
+>;
+
+type test_diagonal_yellow_win_actual = Connect4<
+  {
+    board: [
+      ['  ', '  ', '  ', '  ', '  ', '  ', '  '],
+      ['  ', '  ', '  ', '  ', '  ', '  ', '  '],
+      ['  ', '  ', '  ', '  ', '  ', '  ', '  '],
+      ['  ', '  ', '🟡', '🔴', '  ', '  ', '  '],
+      ['🔴', '🟡', '🔴', '🔴', '  ', '  ', '  '],
+      ['🟡', '🔴', '🟡', '🟡', '  ', '  ', '  ']
+    ];
+    state: '🟡';
+  },
+  3
+>;
+
+type test_diagonal_yellow_win_expected = {
+  board: [
+    ['  ', '  ', '  ', '  ', '  ', '  ', '  '],
+    ['  ', '  ', '  ', '  ', '  ', '  ', '  '],
+    ['  ', '  ', '  ', '🟡', '  ', '  ', '  '],
+    ['  ', '  ', '🟡', '🔴', '  ', '  ', '  '],
+    ['🔴', '🟡', '🔴', '🔴', '  ', '  ', '  '],
+    ['🟡', '🔴', '🟡', '🟡', '  ', '  ', '  ']
+  ];
+  state: '🟡 Won';
+};
+
+type test_diagonal_yellow_win = Expect<
+  Equal<test_diagonal_yellow_win_actual, test_diagonal_yellow_win_expected>
+>;
+
+type test_draw_actual = Connect4<
+  {
+    board: [
+      ['🟡', '🔴', '🔴', '🟡', '🟡', '🔴', '  '],
+      ['🔴', '🟡', '🟡', '🔴', '🔴', '🟡', '🔴'],
+      ['🟡', '🔴', '🔴', '🟡', '🟡', '🔴', '🟡'],
+      ['🔴', '🟡', '🟡', '🔴', '🔴', '🟡', '🔴'],
+      ['🟡', '🔴', '🔴', '🟡', '🟡', '🔴', '🟡'],
+      ['🔴', '🟡', '🟡', '🔴', '🔴', '🟡', '🔴']
+    ];
+    state: '🟡';
+  },
+  6
+>;
+
+type test_draw_expected = {
+  board: [
+    ['🟡', '🔴', '🔴', '🟡', '🟡', '🔴', '🟡'],
+    ['🔴', '🟡', '🟡', '🔴', '🔴', '🟡', '🔴'],
+    ['🟡', '🔴', '🔴', '🟡', '🟡', '🔴', '🟡'],
+    ['🔴', '🟡', '🟡', '🔴', '🔴', '🟡', '🔴'],
+    ['🟡', '🔴', '🔴', '🟡', '🟡', '🔴', '🟡'],
+    ['🔴', '🟡', '🟡', '🔴', '🔴', '🟡', '🔴']
+  ];
+  state: 'Draw';
+};
+
+type test_draw = Expect<Equal<test_draw_actual, test_draw_expected>>;
