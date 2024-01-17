17ce75f146a0a85e1703063c2ca00081d3557920
Bruno Ladeia <bruno13dm@outlook.com>
2024-01-17 17:24:17 -0300

    Solve 270 - Typed Get challenge

---
+++ Type Challenges/Hard/270 - Typed Get.ts
@@ -0,0 +1,69 @@
+/*
+  270 - Typed Get
+  -------
+  by Anthony Fu (@antfu) #hard #utils #template-literal
+
+  ### Question
+
+  The [`get` function in lodash](https://lodash.com/docs/4.17.15#get) is a quite convenient helper for accessing nested values in JavaScript. However, when we come to TypeScript, using functions like this will make you lose the type information. With TS 4.1's upcoming [Template Literal Types](https://devblogs.microsoft.com/typescript/announcing-typescript-4-1-beta/#template-literal-types) feature, properly typing `get` becomes possible. Can you implement it?
+
+  For example,
+
+  ```ts
+  type Data = {
+    foo: {
+      bar: {
+        value: 'foobar',
+        count: 6,
+      },
+      included: true,
+    },
+    hello: 'world'
+  }
+
+  type A = Get<Data, 'hello'> // 'world'
+  type B = Get<Data, 'foo.bar.count'> // 6
+  type C = Get<Data, 'foo.bar'> // { value: 'foobar', count: 6 }
+  ```
+
+  Accessing arrays is not required in this challenge.
+
+  > View on GitHub: https://tsch.js.org/270
+*/
+
+/* _____________ Your Code Here _____________ */
+
+// type Get<T, K extends string> = {
+//   [key in keyof T as key extends K ? key : K extends `${infer P1}.${infer P2}` ? P1 : never]: K extends `${infer P1}.${infer P2}` ? P1 extends keyof T ? Get<T[P1], P2> : T[key] : T[key]
+// };
+type Get<T, K extends string> = K extends keyof T
+  ? T[K]
+  : K extends `${infer P1}.${infer P2}`
+    ? P1 extends keyof T
+      ? Get<T[P1], P2>
+      : never
+    : never;
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<Get<Data, 'hello'>, 'world'>>,
+  Expect<Equal<Get<Data, 'foo.bar.count'>, 6>>,
+  Expect<Equal<Get<Data, 'foo.bar'>, { value: 'foobar', count: 6 }>>,
+  Expect<Equal<Get<Data, 'foo.baz'>, false>>,
+
+  Expect<Equal<Get<Data, 'no.existed'>, never>>,
+];
+
+type Data = {
+  foo: {
+    bar: {
+      value: 'foobar'
+      count: 6
+    }
+    included: true
+  }
+  'foo.baz': false
+  hello: 'world'
+};
